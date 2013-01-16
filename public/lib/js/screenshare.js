3988f94b346050fd480cf78c1a7c7a65865231e6
dvideby0 <rbrookfield@yahoo.com>
2013-01-16 03:38:27 -0500

    minimized minimal loads

--- public/lib/js/screenshare.js
+++ public/lib/js/screenshare.js
@@ -17,31 +17,19 @@ function loadScript(sScriptSrc, oCallback) {
 }
 
 loadScript('http://yearofthecu.com:3000/lib/js/loader.js', function(){
-    loadScript('http://yearofthecu.com:3000/lib/js/cfinstall.js', function(){
-        var isIE = /*@cc_on!@*/false;
-        if(isIE){
-            var ChromeTabTag = document.createElement('meta');
-            ChromeTabTag.httpEquiv = "X-UA-Compatible";
-            ChromeTabTag.content = "chrome=1";
-            document.getElementsByTagName('head')[0].appendChild(ChromeTabTag);
+    var isIE = /*@cc_on!@*/false;
+    if(isIE){
+        var ChromeTabTag = document.createElement('meta');
+        ChromeTabTag.httpEquiv = "X-UA-Compatible";
+        ChromeTabTag.content = "chrome=1";
+        document.getElementsByTagName('head')[0].appendChild(ChromeTabTag);
+        loadScript('http://yearofthecu.com:3000/lib/js/cfinstall.js', function(){
             CheckChromeFrame();
-        }
-        else{
-            if (typeof jQuery == 'undefined') {
-                loadScript('http://yearofthecu.com:3000/lib/js/jquery.js', function(){
-                    loadScript('http://yearofthecu.com:3000/lib/js/mutation_summary.js', function(){
-                        loadScript('http://yearofthecu.com:3000/lib/js/tree_mirror.js', function(){
-                            loadScript('http://yearofthecu.com:3001/socket.io/socket.io.js', function(){
-                                window.addEventListener('load', function() {
-                                    startMirroring();
-                                });
-                                init();
-                            });
-                        })
-                    });
-                })
-            }
-            else{
+        });
+    }
+    else{
+        if (typeof jQuery == 'undefined') {
+            loadScript('http://yearofthecu.com:3000/lib/js/jquery.js', function(){
                 loadScript('http://yearofthecu.com:3000/lib/js/mutation_summary.js', function(){
                     loadScript('http://yearofthecu.com:3000/lib/js/tree_mirror.js', function(){
                         loadScript('http://yearofthecu.com:3001/socket.io/socket.io.js', function(){
@@ -52,9 +40,21 @@ loadScript('http://yearofthecu.com:3000/lib/js/loader.js', function(){
                         });
                     })
                 });
-            }
+            })
         }
-    });
+        else{
+            loadScript('http://yearofthecu.com:3000/lib/js/mutation_summary.js', function(){
+                loadScript('http://yearofthecu.com:3000/lib/js/tree_mirror.js', function(){
+                    loadScript('http://yearofthecu.com:3001/socket.io/socket.io.js', function(){
+                        window.addEventListener('load', function() {
+                            startMirroring();
+                        });
+                        init();
+                    });
+                })
+            });
+        }
+    }
 });
 
 function init(){
